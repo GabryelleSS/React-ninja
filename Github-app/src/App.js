2fdeff3d1853be707063b809e3b66d382642f731
Gabryelle SS <gabryelle-gaby@hotmail.com>
2020-03-22 21:39:21 -0300

    feat: leaving dynamic URL

--- Github-app/src/App.js
+++ Github-app/src/App.js
@@ -16,13 +16,20 @@ class App extends Component {
     }
   }
 
+  getGitHubApiUrl(userName, type) {
+    const internalUserName = userName ? `/${userName}` : '';
+    const internalType = type ? `/${type}` : '';
+
+    return `https://api.github.com/users${internalUserName}${internalType}`;
+  }
+
   handleSearch(evt) {
     const value = evt.target.value;
     const keyCode = evt.which || evt.keyCode;
     const ENTER = 13;
 
     if (keyCode === ENTER) {
-      ajax().get(`https://api.github.com/users/${value}`)
+      ajax().get(this.getGitHubApiUrl(value))
         .then((result) => {
           this.setState({
             userInfo: {
@@ -32,17 +39,21 @@ class App extends Component {
               repos: result.public_repos,
               followers: result.followers,
               following: result.following
-            }
+            },
+            repos: [],
+            starred: []
           });
-
-          console.log(result)
         });
     }
   }
 
   getRepos(type) {
+
     return(e) => {
-      ajax().get(`https://api.github.com/users/GabryelleSS/${type}`)
+      
+      const userName = this.state.userInfo.login;
+
+      ajax().get(this.getGitHubApiUrl(userName, type))
       .then(result => {
         this.setState({
           [type]: [{
